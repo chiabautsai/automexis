6e5d862006de1fd4ef74390b4943cb8f94fd75e8
chiabautsai <chiabautsai@gmail.com>
2023-08-04 21:11:12 +0800

    added remove children to asset script

--- src/assets.js
+++ src/assets.js
@@ -1,10 +1,19 @@
+const script = `
+const curentElement = document.currentScript;
+const currentPost = curentElement.closest('.t_row');
+const avatar = currentPost.querySelector('.avatar > img');
+const postContent = curentElement.closest('td.line');
+
+((parent) => { for (let i = parent.children.length - 2; i >= 0; i--) {
+  if (i === 18 || i >= 7) {
+      postContent.removeChild(parent.children[i]);
+  }
+}}) (postContent);
+`;
+
 const data = {
   script: script,
 };
 
-const script = `
-const currentPost = document.currentScript.closest('.t_row');
-const avatar = currentPost.querySelector('.avatar > img');
-`;
 
 export default data;
